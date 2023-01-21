c25695c7b8f05dce4302b42b7821623e322ba67c
theGeekkit <sonofgondolin16@gmail.com>
2023-01-21 14:07:29 -0600

    Bad Medicine 1-21-23

---
+++ lib/west_game.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "west_game/version"
+
+module WestGame
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/west_game/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module WestGame
+  VERSION = "0.1.0"
+end
